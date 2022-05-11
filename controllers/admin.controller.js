7acba18faff2e01114bf2653c9c2e58eeb9aef57
JulianTM <jpachonl99@gmail.com>
2022-05-11 12:55:11 -0500

    admin producto conexion con JSON

---
+++ controllers/admin.controller.js
@@ -0,0 +1,31 @@
+import { clientServices } from "../services/client-service.js";
+
+//Creando la card del producto
+const MostrarProductosAdmin = (nombre, precio, descripcion, imagen, id, categoria) => {
+  //Creando el div que guarda todo el card
+  const cardProducto = document.createElement("div");
+  cardProducto.className = "producto__card";
+  const contenido = `
+  <div class="producto__card__imagen" style="background-Image: url(${imagen})">
+    <a class="boton-eliminar" href="#"><img src="./assets/img/eliminar-boton.svg" alt="boton eliminar"></a>
+    <a class="boton-editar" href="#"><img src="./assets/img/editar-boton.svg" alt="boton editar"></a>
+  </div>
+  <h3 class="producto__card__titulo">${nombre}</h3>
+  <p class="producto__card__precio">${precio}</p>
+  <a class="producto__card__boton" href="./ver-producto.html">Ver producto</a>
+  `
+  cardProducto.innerHTML = contenido;
+  return cardProducto;
+}
+
+//Capturando la seccion para mostrar los productos
+const productosAdmin = document.querySelector("[data-productos-admin]");
+
+//Recorrer los datos traidos del JSON
+clientServices.listaProductos().then(data => {
+  data.forEach(({nombre, precio, descripcion, imagen, id, categoria}) => {
+    //Imprimir datos en el index
+    const nuevoProducto = MostrarProductosAdmin(nombre, precio, descripcion, imagen, id, categoria);
+    productosAdmin.appendChild(nuevoProducto);
+  });
+}).catch(error => alert("ocurrio un error"));
