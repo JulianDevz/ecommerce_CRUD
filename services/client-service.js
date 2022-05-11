b278d488beb54ad2c24b41156077f3c9fcc7497c
JulianTM <jpachonl99@gmail.com>
2022-05-10 21:59:30 -0500

    mostrando productos de JSON

--- services/client-service.js
+++ services/client-service.js
@@ -1,6 +1,12 @@
 //Conexion
 const listaProductos = () => fetch('https://ecommerce-juliandevz.herokuapp.com/productos').then(respuesta => respuesta.json());
 
+//Detalles del producto por ID
+const detalleProducto = async (id) => {
+  return fetch(`https://ecommerce-juliandevz.herokuapp.com/productos/${id}`).then( respuesta => respuesta.json());
+}
+
 export const clientServices = {
-  listaProductos
+  listaProductos,
+  detalleProducto
 }
